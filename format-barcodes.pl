b36508bccdb1abf7d069240a4c9f60d2a9b0d20b
Matthew Slowe <foo@mafoo.org.uk>
2019-11-30 13:27:42 +0000

    barcodes

--- format-barcodes.pl
+++ format-barcodes.pl
@@ -2,23 +2,8 @@
 # Script to generate ParkRun datafiles for upload
 # Lawrence Billson, 2015, Creative Commons license
 # 
-# Reset the USB modules
-system("killall -9 python");
-system("rmmod -f opticon; rmmod -f usbserial");
-system("modprobe opticon");
-#
 
-
-# Barcode routine 
-# Need to generate a date stamp for our barcode file
-#$time = time();
-$time = `date +%a-%b-%d-%H:%M:%S.txt`;
-$barcodefile = "/www/files/parkrun_barcode_$time";
-open(BARCODEFILE,">$barcodefile");
-
-
-@data =  `/www/cgi-bin/cs1504.py`;
-foreach $line (@data) {
+while(my $line = <>) {
 	chomp($line);
 	($code, $data, $date)  = split(',',$line);
 
@@ -44,21 +29,14 @@ foreach $line (@data) {
 	if ($data =~ /^A/) {
 		if ($athelete) {
 			# We've already got athelete data, dump the place
-			print BARCODEFILE "$athelete,,$pdate\n";
+			print "$athelete,,$pdate\n";
 			}
 		$athelete = $data;
 		}
 	if ($data =~ /^P/) {
 		# Is there athelete data?, if not, print some stuff
-		print BARCODEFILE "$athelete,$data,$pdate\n";
+		print "$athelete,$data,$pdate\n";
 		$athelete = "";
 		}
 	}
-close(BARCODEFILE);
-
-# delete any zero length files left behind
-`find /www/files/ -size 0 -type f -exec rm {} \\; > /tmp/rm 2>&1`;
-
-
-exit(0);
 
